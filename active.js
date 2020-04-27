cdf248b7ca78a9babb36487eb1560a956074ebb7
moatadil <moatadil.2013@gmail.com>
2020-04-27 15:08:23 +0300

    iframe correct

--- active.js
+++ active.js
@@ -1,10 +1,14 @@
-// Add active class to the current button (highlight it)
-var header = document.getElementById("myDIV");
-var btns = header.getElementsByClassName("buttonMenu");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-  var current = document.getElementsByClassName("act");
-  current[0].className = current[0].className.replace(" act", "");
-  this.className += " act";
-  });
-}
+$(function(){
+    'use strict';
+  // Add active class to the current button (highlight it)
+  var header = document.getElementById("myDIV");
+  var btns = header.getElementsByClassName("buttonMenu");
+  for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("act");
+    current[0].className = current[0].className.replace(" act", "");
+    this.className += " act";
+    });
+  }
+ 
+});
